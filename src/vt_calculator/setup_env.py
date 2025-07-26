ad4e0f377d8accc69c92f5556af757523987b680
Eon Kim <kimiron518@gmail.com>
2025-07-26 22:14:33 +0000

    Refactor environment setup by removing logging configurations

--- src/vt_calculator/setup_env.py
+++ src/vt_calculator/setup_env.py
@@ -1,31 +1,9 @@
-#!/usr/bin/env python3
-"""
-Environment setup script to disable Hugging Face warnings and logging.
-Run this before importing any transformers modules.
-"""
-
 import os
 import warnings
-import logging
 
 
 def setup_quiet_environment():
-    """
-    Set up environment variables and logging to minimize Hugging Face output.
-    """
-    # Environment variables to control HF behavior
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-    os.environ["TRANSFORMERS_VERBOSITY"] = "error"
-    os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
-    os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = "1"
-
-    # Disable Python warnings
     warnings.filterwarnings("ignore")
-
-    # Set logging level to ERROR for common libraries
-    logging.getLogger("transformers").setLevel(logging.ERROR)
-    logging.getLogger("huggingface_hub").setLevel(logging.ERROR)
-    logging.getLogger("torch").setLevel(logging.ERROR)
 
     try:
         from huggingface_hub.utils import disable_progress_bars
---
+++ src/vt_calculator/vlm_models.py
@@ -0,0 +1,16 @@
+class VLMBase:
+    image_token: str = ""
+    image_start_token: str = ""
+    image_end_token: str = ""
+
+
+class Qwen2_5_VL(VLMBase):
+    image_token: str = "<|image_pad|>"
+    image_start_token: str = "<|im_start|>"
+    image_end_token: str = "<|im_end|>"
+
+
+__all__ = [
+    "VLMBase",
+    "Qwen2_5_VL",
+]
