1c0063a80b8e5967cde07acefffa5530838a0160
Eon Kim <kimiron518@gmail.com>
2025-07-27 00:08:45 +0000

    Refactor code

--- src/vt_calculator/vlm_models.py
+++ src/vt_calculator/vlm_models.py
@@ -1,9 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class VLMBase:
     image_token: str = ""
     image_start_token: str = ""
     image_end_token: str = ""
 
 
+@dataclass
 class Qwen2_5_VL(VLMBase):
     image_token: str = "<|image_pad|>"
     image_start_token: str = "<|im_start|>"
