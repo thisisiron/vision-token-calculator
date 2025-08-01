d8971c02b0f3301824e6e4e0179fd3bbe628d3ae
Eon Kim <kimiron518@gmail.com>
2025-08-01 21:52:38 +0000

    Refactor code

--- src/vt_calculator/analysts/analyst.py
+++ src/vt_calculator/analysts/analyst.py
@@ -1,41 +1,6 @@
-from dataclasses import dataclass
-import math
+from ..utils import smart_resize
 
 
-def smart_resize(
-    height: int,
-    width: int,
-    factor: int = 28,
-    min_pixels: int = 56 * 56,
-    max_pixels: int = 14 * 14 * 4 * 1280,
-):
-    """Rescales the image so that the following conditions are met:
-
-    1. Both dimensions (height and width) are divisible by 'factor'.
-
-    2. The total number of pixels is within the range ['min_pixels', 'max_pixels'].
-
-    3. The aspect ratio of the image is maintained as closely as possible.
-
-    """
-    if max(height, width) / min(height, width) > 200:
-        raise ValueError(
-            f"absolute aspect ratio must be smaller than 200, got {max(height, width) / min(height, width)}"
-        )
-    h_bar = round(height / factor) * factor
-    w_bar = round(width / factor) * factor
-    if h_bar * w_bar > max_pixels:
-        beta = math.sqrt((height * width) / max_pixels)
-        h_bar = max(factor, math.floor(height / beta / factor) * factor)
-        w_bar = max(factor, math.floor(width / beta / factor) * factor)
-    elif h_bar * w_bar < min_pixels:
-        beta = math.sqrt(min_pixels / (height * width))
-        h_bar = math.ceil(height * beta / factor) * factor
-        w_bar = math.ceil(width * beta / factor) * factor
-    return h_bar, w_bar
-
-
-@dataclass
 class VLMAnalyst:
     def __init__(self, processor):
         self.processor = processor
@@ -47,8 +12,7 @@ class VLMAnalyst:
         pass
 
 
-@dataclass
-class Qwen2_5_VLAnalyst(VLMAnalyst):
+class Qwen2VLAnalyst(VLMAnalyst):
     def __init__(self, processor):
         super().__init__(processor)
 
@@ -78,6 +42,11 @@ class Qwen2_5_VLAnalyst(VLMAnalyst):
         return num_patches // self.merge_size**2
 
 
+class Qwen2_5_VLAnalyst(Qwen2VLAnalyst):
+    pass
+
+
 __all__ = [
+    "Qwen2VLAnalyst",
     "Qwen2_5_VLAnalyst",
 ]
